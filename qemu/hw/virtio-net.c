5d0e5fef9b0424fd3f9e8889c7f363b3cd9a76af
Stephen Finucane <stephen.finucane@intel.com>
2014-04-29 17:07:07 +0100

    qemu: Remove unused function
    A static function was no used due to modifications made in Intel(R)
    DPDK vSwitch. Remove it and fix the "unused-function" warning.
    Additionally, remove the commented block that had rendered this
    function "unused" - the block itself is available in history if
    needed.
    
    Change-Id: Icccc01ae27a953c25c9ba7ab9c36af3bd0168657
    Signed-off-by: Stephen Finucane <stephen.finucane@intel.com>
    Reviewed-by: Mark D. Gray <mark.d.gray@intel.com>

--- qemu/hw/virtio-net.c
+++ qemu/hw/virtio-net.c
@@ -750,57 +750,6 @@ static void receive_header(VirtIONet *n, const struct iovec *iov, int iov_cnt,
     }
 }
 
-static int receive_filter(VirtIONet *n, const uint8_t *buf, int size)
-{
-    static const uint8_t bcast[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
-    static const uint8_t vlan[] = {0x81, 0x00};
-    uint8_t *ptr = (uint8_t *)buf;
-    int i;
-
-    if (n->promisc)
-        return 1;
-
-    ptr += n->host_hdr_len;
-
-    if (!memcmp(&ptr[12], vlan, sizeof(vlan))) {
-        int vid = be16_to_cpup((uint16_t *)(ptr + 14)) & 0xfff;
-        if (!(n->vlans[vid >> 5] & (1U << (vid & 0x1f))))
-            return 0;
-    }
-
-    if (ptr[0] & 1) { // multicast
-        if (!memcmp(ptr, bcast, sizeof(bcast))) {
-            return !n->nobcast;
-        } else if (n->nomulti) {
-            return 0;
-        } else if (n->allmulti || n->mac_table.multi_overflow) {
-            return 1;
-        }
-
-        for (i = n->mac_table.first_multi; i < n->mac_table.in_use; i++) {
-            if (!memcmp(ptr, &n->mac_table.macs[i * ETH_ALEN], ETH_ALEN)) {
-                return 1;
-            }
-        }
-    } else { // unicast
-        if (n->nouni) {
-            return 0;
-        } else if (n->alluni || n->mac_table.uni_overflow) {
-            return 1;
-        } else if (!memcmp(ptr, n->mac, ETH_ALEN)) {
-            return 1;
-        }
-
-        for (i = 0; i < n->mac_table.first_multi; i++) {
-            if (!memcmp(ptr, &n->mac_table.macs[i * ETH_ALEN], ETH_ALEN)) {
-                return 1;
-            }
-        }
-    }
-
-    return 0;
-}
-
 static ssize_t virtio_net_receive(NetClientState *nc, const uint8_t *buf, size_t size)
 {
     VirtIONet *n = qemu_get_nic_opaque(nc);
@@ -818,10 +767,7 @@ static ssize_t virtio_net_receive(NetClientState *nc, const uint8_t *buf, size_t
     if (!virtio_net_has_buffers(q, size + n->guest_hdr_len - n->host_hdr_len)) {
         return 0;
     }
-/*
-    if (!receive_filter(n, buf, size))
-        return size;
-*/
+
     offset = i = 0;
 
     while (offset < size) {
