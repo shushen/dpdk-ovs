3db4f3768215d66832031687dd5733c388af7a75
Mark D. Gray <mark.d.gray@intel.com>
2014-07-08 17:02:10 +0100

    datapath: Introducing 'ovdk_flow'
    'ovdk_flow.c' provides the functionality required to extract
    a 'struct ovdk_flow_key' from a packet and other related
    helper functions.
    
    Change-Id: I9d0df80262c4e3bf930f1a6ba7915ed29b83a04c
    Signed-off-by: Mark D. Gray <mark.d.gray@intel.com>
    Signed-off-by: Kevin Traynor <kevin.traynor@intel.com>
    Signed-off-by: Eimear Morrissey <eimear.morrissey@intel.com>
    Signed-off-by: Meghan Halton <meghan.halton@intel.com>
    Signed-off-by: Mark B. Kavanagh <mark.b.kavanagh@intel.com>
    Signed-off-by: Stephen Finucane <stephen.finucane@intel.com>
    Signed-off-by: Ian Stokes <ian.stokes@intel.com>
    Signed-off-by: Ciara Loftus <ciara.loftus@intel.com>

---
+++ openvswitch/datapath/dpdk/ovdk_flow.h
@@ -0,0 +1,60 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright(c) 2010-2014 Intel Corporation. All rights reserved.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef __OVDK_FLOW_H_
+#define __OVDK_FLOW_H_
+
+#include <rte_config.h>
+#include <rte_mbuf.h>
+#include <rte_ether.h>
+#include <rte_pipeline.h>
+
+#include "ovdk_flow_types.h"
+
+#define TCP_FLAG_MASK                  (0x3F)
+/* Calculates the IPv4 header size using the Internet Header Length field (IHL).
+ * This field contains the number of 32-bit words in the header. */
+#define IPV4_HEADER_SIZE(ipv4_hdr) \
+            (((ipv4_hdr)->version_ihl & 0x0F) * sizeof(uint32_t))
+#define TCP_HDR_FROM_PKT(pkt) (struct tcp_hdr*)\
+               (rte_pktmbuf_mtod(pkt, unsigned char *) + \
+               sizeof(struct ether_hdr) + \
+               IPV4_HEADER_SIZE(rte_pktmbuf_mtod(pkt, struct ipv4_hdr*)))
+
+int flow_keys_extract(struct rte_mbuf **pkts, uint32_t num_pkts,
+                      uint64_t *pkts_mask, __attribute__((unused)) void *arg);
+
+uint64_t ovs_flow_used_time(uint64_t curr_tsc, uint64_t flow_tsc);
+
+#endif /* __OVDK_FLOW_H_ */
