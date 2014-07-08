1ec7e01b8a60c2499c1d25c7c8f547d0dea823fb
Mark D. Gray <mark.d.gray@intel.com>
2014-07-08 16:49:22 +0100

    datapath: Add infrastructure for different port types
    Each port associated with the datapath is called a vport and
    has an individual unique index called the 'vportid'. Currently,
    this is equivalent to the concept of an Open vSwitch odp port
    number.
    
    Unlike previous versions of the vswitch, the ranges of these
    numbers can be arbitrarily defined at compile time.
    'ovdk_vport_types.h' defines these ranges and as such, needs to
    be shared with the vswitchd.
    
    At initialization time, resources for all vports must be
    allocated. Then, as the vswitchd adds ports these vports
    get added to the rte_pipeline. Each vport is described
    in a memzone which can be shared to a guest if required. The
    data in this memzone is described in 'ovdk_vport_info.h' and
    can be accessed by ovs_vport.
    
    If a port works with the DPDK packet framework, it should be
    reasonably easy to enable this port with the vswitch.
    
    Change-Id: Ie45378238cc401343ac850d05f9b8eb13e87adda
    Signed-off-by: Mark D. Gray <mark.d.gray@intel.com>
    Signed-off-by: Kevin Traynor <kevin.traynor@intel.com>
    Signed-off-by: Eimear Morrissey <eimear.morrissey@intel.com>
    Signed-off-by: Meghan Halton <meghan.halton@intel.com>
    Signed-off-by: Mark B. Kavanagh <mark.b.kavanagh@intel.com>
    Signed-off-by: Stephen Finucane <stephen.finucane@intel.com>
    Signed-off-by: Ian Stokes <ian.stokes@intel.com>
    Signed-off-by: Ciara Loftus <ciara.loftus@intel.com>

---
+++ openvswitch/datapath/dpdk/ovdk_vport_info.h
@@ -0,0 +1,170 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright(c) 2010-2014 Intel Corporation. All rights reserved.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef __OVDK_VPORT_INFO_H_
+#define __OVDK_VPORT_INFO_H_
+
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include <rte_mbuf.h>
+#include <rte_memory.h>
+#include <rte_memzone.h>
+#include <rte_pipeline.h>
+#include <rte_port_ethdev.h>
+#include <rte_port_source_sink.h>
+
+#include "rte_port_vhost.h"
+#include "rte_port_ivshm.h"
+
+#include "ovdk_vport_types.h"
+
+#define MZ_VPORT_INFO           "OVS_vport_info"
+#define rpl_strnlen strnlen
+
+struct vport_kni_fifo_names {
+	char tx[RTE_MEMZONE_NAMESIZE];
+	char rx[RTE_MEMZONE_NAMESIZE];
+	char alloc[RTE_MEMZONE_NAMESIZE];
+	char free[RTE_MEMZONE_NAMESIZE];
+	char req[RTE_MEMZONE_NAMESIZE];
+	char resp[RTE_MEMZONE_NAMESIZE];
+	char sync[RTE_MEMZONE_NAMESIZE];
+};
+
+struct vport_client_ring_names {
+	char rx[RTE_RING_NAMESIZE];
+	char tx[RTE_RING_NAMESIZE];
+	char free[RTE_RING_NAMESIZE];
+	char alloc[RTE_RING_NAMESIZE];
+};
+
+struct vport_phy {
+	struct rte_port_ethdev_writer_params port_writer_ethdev_params[RTE_MAX_LCORE];
+	struct rte_port_ethdev_reader_params port_reader_ethdev_params;
+};
+
+struct vport_client {
+	struct vport_client_ring_names ring_names;
+	struct rte_port_ivshm_writer_params port_writer_client_params;
+	struct rte_port_ivshm_reader_params port_reader_client_params;
+};
+
+struct vport_kni {
+	struct vport_kni_fifo_names fifo_names;
+	uint8_t index;
+};
+
+struct vport_veth {
+	uint8_t index;
+};
+
+struct vport_vhost {
+	struct rte_port_vhost_writer_params port_writer_vhost_params[RTE_MAX_LCORE];
+	struct rte_port_vhost_reader_params port_reader_vhost_params;
+	struct virtio_net *dev;
+	uint8_t index;
+};
+
+struct vport_bridge {
+	struct rte_port_source_params port_reader_source_params;
+};
+
+struct vport_info {
+	enum ovdk_vport_type __rte_cache_aligned type;
+	char __rte_cache_aligned name[OVDK_MAX_VPORT_NAMESIZE];
+	bool __rte_cache_aligned enabled;
+	uint32_t vportid;
+	union {
+		struct vport_phy phy;
+		struct vport_client client;
+		struct vport_kni kni;
+		struct vport_veth veth;
+		struct vport_vhost vhost;
+		struct vport_bridge bridge;
+	};
+	struct rte_pipeline_port_in_params port_in_params;
+	struct rte_pipeline_port_out_params port_out_params[RTE_MAX_LCORE];
+	/*
+	 * 'port_in_id' is the id assigned when this vport is added as an
+	 * in-port to an rte_pipeline
+	 */
+	uint32_t port_in_id;
+	/*
+	 * 'port_out_id' is the id assigned when this vport is added as an
+	 * out-port to an rte_pipeline. As a vport gets added to every
+	 * rte_pipeline, each rte_pipeline may assign a different port id.
+	 * Therefore we need an array
+	 */
+	uint32_t port_out_id[RTE_MAX_LCORE];
+};
+
+/*
+ * Check if port_name is a vport valid name.
+ *
+ * A valid port_name is an alphanumeric string whose length is not greater
+ * than max vport name length.
+ *
+ * If port_name is a valid vport name, returns 0. Otherwise, returns -1.
+ */
+static inline int
+vport_is_valid_name(const char *port_name)
+{
+	size_t length = 0;
+	unsigned i = 0;
+
+	if (port_name == NULL)
+		return -EINVAL;
+
+	length = strnlen(port_name, OVDK_MAX_VPORT_NAMESIZE + 1);
+
+	/*
+	 * If port_name length is equal or greater than VPORT_INFO_NAMESZ+1
+	 * strlen will return VPORT_INFO_NAMESZ+1. In that case we know
+	 * port_name is too big for a valid port name. strlen does take into
+	 * account the final \0 byte but it does not return it as part of the
+	 * string length.
+	 */
+	if (length == OVDK_MAX_VPORT_NAMESIZE + 1)
+		return -EINVAL;
+
+	for (i = 0; i < length; i++)
+		if (!isalnum(port_name[i]))
+			return -EINVAL;
+
+	return 0;
+}
+
+#endif /* __OVDK_VPORT_INFO_H_ */
